8e4973f264005142d8b9c72712830e1856646f84
CodeBeast0 <161934121+CodeBeast0@users.noreply.github.com>
2026-01-17 10:00:22 +0100

    fix auth jwt

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
+
 const allowedOrigins = [
   "http://localhost:5173",
   "https://fley-two.vercel.app"
@@ -22,7 +22,8 @@ if (process.env.FRONTEND_URL) {
 
 app.use(cors({
   origin: allowedOrigins,
-  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 // Routes
